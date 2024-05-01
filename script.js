95fef39551b3a0331bbcc807729eb6f1716bceb0
nadhilahaz <hazratinadhilah@gmail.com>
2024-05-01 21:01:44 +0700

    button dan font

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function encrypt() {
   var inputText = document.getElementById("inputText").value;
-  var password = prompt("Masukkan kata sandi untuk enkripsi:");
+  var password = document.getElementById("passwordInput").value; // Mengambil kunci dari input kotak bawah
   var encryptedText = CryptoJS.AES.encrypt(inputText, password).toString();
   document.getElementById("outputText").value = encryptedText;
 }
@@ -8,9 +8,15 @@ function encrypt() {
 // Fungsi untuk melakukan dekripsi
 function decrypt() {
   var encryptedText = document.getElementById("outputText").value;
-  var password = prompt("Masukkan kata sandi untuk dekripsi:");
+  var password = document.getElementById("passwordInput").value; // Mengambil kunci dari input kotak bawah
   var decryptedText = CryptoJS.AES.decrypt(encryptedText, password).toString(
     CryptoJS.enc.Utf8
   );
   document.getElementById("outputText").value = decryptedText;
+}
+
+function resetAll() {
+  document.getElementById("inputText").value = ""; // Menghapus nilai dari input teks
+  document.getElementById("passwordInput").value = ""; // Menghapus nilai dari input kunci
+  document.getElementById("outputText").value = ""; // Menghapus nilai dari output teks
 }
