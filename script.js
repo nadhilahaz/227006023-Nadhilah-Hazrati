452f4e40417ff935a46985b43af6a76dd0d2131e
nadhilahaz <hazratinadhilah@gmail.com>
2024-05-03 09:31:00 +0700

    keterangan

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
+// Fungsi untuk mengenkripsi teks menggunakan Kode Rives 4 dan sebuah kunci
 function encrypt() {
   var inputText = document.getElementById("inputText").value;
-  var password = document.getElementById("passwordInput").value; // Mengambil kunci dari input kotak bawah
-  var encryptedText = CryptoJS.AES.encrypt(inputText, password).toString();
-  document.getElementById("outputText").value = encryptedText;
+  var key = parseInt(document.getElementById("key").value); // Mengonversi kunci string menjadi integer
+  var outputText = "";
+  for (var i = 0; i < inputText.length; i++) {
+    var charCode = inputText.charCodeAt(i);
+    outputText += String.fromCharCode(charCode + key); // Geser karakter
+  }
+  document.getElementById("outputText").value = outputText;
+  document.getElementById("outputNote").innerText =
+    "Teks telah dienkripsi dengan kunci: " + key;
 }
 
-// Fungsi untuk melakukan dekripsi
+// Fungsi untuk mendekripsi teks yang telah dienkripsi menggunakan Kode Rives 4 dan sebuah kunci
 function decrypt() {
-  var encryptedText = document.getElementById("outputText").value;
-  var password = document.getElementById("passwordInput").value; // Mengambil kunci dari input kotak bawah
-  var decryptedText = CryptoJS.AES.decrypt(encryptedText, password).toString(
-    CryptoJS.enc.Utf8
-  );
-  document.getElementById("outputText").value = decryptedText;
+  var inputText = document.getElementById("inputText").value;
+  var key = parseInt(document.getElementById("key").value); // Mengonversi kunci string menjadi integer
+  var outputText = "";
+  for (var i = 0; i < inputText.length; i++) {
+    var charCode = inputText.charCodeAt(i);
+    outputText += String.fromCharCode(charCode - key); // Memutar kembali geseran
+  }
+  document.getElementById("outputText").value = outputText;
+  document.getElementById("outputNote").innerText =
+    "Teks telah didekripsi dengan kunci: " + key;
 }
 
-function resetAll() {
-  document.getElementById("inputText").value = ""; // Menghapus nilai dari input teks
-  document.getElementById("passwordInput").value = ""; // Menghapus nilai dari input kunci
-  document.getElementById("outputText").value = ""; // Menghapus nilai dari output teks
+// Fungsi untuk mengatur ulang bidang masukan dan keluaran
+function resetFields() {
+  document.getElementById("inputText").value = "";
+  document.getElementById("outputText").value = "";
+  document.getElementById("key").value = "";
+  document.getElementById("outputNote").innerText = ""; // Hapus keterangan
 }
+
+// Pemantau acara untuk tombol reset
+document.getElementById("resetButton").addEventListener("click", resetFields);
